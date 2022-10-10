c54a8f021c12f101ad6f7c32324d7c6c82826b30
Simon Plakolb <s.plakolb@gmail.com>
2022-10-10 16:35:06 +0200

    nix: Add derivation to flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,38 @@
 {
   description = "LORA - the LibreOffice Recovery Assistant";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      drv = pkgs.stdenv.mkDerivation {
+        pname = "lora";
+        version = "0.0.1dev";
+        src = ./.;
+        buildInputs = with pkgs; [ bash yq libreoffice ];
+        #nativeBuildInputs = with pkgs; [ makeWrapper ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp lora.sh $out/bin/lora.sh
+          '';
+          #wrapProgram $out/bin/lora.sh \
+            #--prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bash pkgs.yq pkgs.libreoffice ]}
+        #'';
+      };
+
     in {
+      apps.default = {
+        type = "app";
+        program = "${drv}/bin/lora.sh";
+      };
+      packages.default = drv;
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = [ pkgs.bashInteractive ];
-        buildInputs = with pkgs; [ yq libreoffice ];
+        nativeBuildInputs = with pkgs; [ bashInteractive ];
+        buildInputs = with pkgs; [ bash yq libreoffice ];
       };
     });
 }
